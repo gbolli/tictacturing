ad16ef005c9282d167b6057d535cf86a566af8f4
Gord Bolli <gord@bolli.ca>
2017-08-18 14:27:39 -0700

    added profile table and styling

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -4,11 +4,83 @@ import {Container, Name, GameListHeader, GameList, GameRecord, Column, ColumnLab
 
 class Profile extends Component {
 
+  static defaultProps = {
+    user: {
+      email: 'USER_EMAIL',
+      games: [
+        {
+          winner: true,
+          createdAt: '12/25/2016',
+          id: '0001'
+        },
+        {
+          winner: true,
+          createdAt: '12/26/2016',
+          id: '0002'
+        },
+        {
+          winner: true,
+          createdAt: '12/27/2016',
+          id: '0003'
+        }
+      ]  // games
+    }  // user
+  }
+
+  get records() {
+    return this.props.user.games.map( (game, index) => {
+      return (
+        <GameRecord
+          key={index}
+          index={index}
+        >
+          <Column>
+            {(game.winner) ? 'Won!' : "Didn't win"}
+          </Column>
+          <Column>
+            "Robot"
+          </Column>
+          <Column>
+            "No"
+          </Column>
+          <Column>
+            {game.createdAt}
+          </Column>
+        </GameRecord>
+      )
+    })
+  }
+
   render() {
+    let {email} = this.props.user
+
     return (
-      <div>
-        <h2>Profile!</h2>
-      </div>
+      <Container>
+        <Name>
+          {email}
+        </Name>
+        <GameList>
+          <GameListHeader>
+            My Games
+          </GameListHeader>
+          <ColumnLabels>
+            <Column>
+              Outcome  
+            </Column>
+            <Column>
+              Guess  
+            </Column>
+            <Column>
+              Guessed Correctly  
+            </Column>
+            <Column>
+              Date  
+            </Column>
+          </ColumnLabels>
+          {this.records}
+        </GameList>
+
+      </Container>
       )
   }
 }
